f74bde29f425aedc39d044dcf19cc911d1b7d32f
Michael Aangel <kameku01@gmail.com>
2023-01-21 09:59:40 -0500

    Bot gmt funcional

---
+++ app.js
@@ -0,0 +1,89 @@
+const {
+    createBot,
+    createProvider,
+    createFlow,
+    addKeyword,
+} = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const JsonFileAdapter = require('@bot-whatsapp/database/mock')
+
+
+
+const flowVerifyPay = addKeyword(['1', 'pagos', 'quiero saber cuando me pagan', 'cuando pagan'])
+    .addAnswer("En este link pododras consultar por la placa\nde tu vehiculo el estado de tu pago.")
+    .addAnswer("https://appgmt.com")
+    .addAnswer("Solo debes escribir *la placa de tu vehiculo* \n y dar click en *Consultar*", {
+        media:'https://netikom.com.au/novena/gmtapp.jpg'
+    })
+    
+const flowCertificados = addKeyword(['2','certificados','necesito el certificado'])
+    .addAnswer('Para obtener el certificados\n de ingresos y retenciones')
+    .addAnswer('Envia una solicitud al correo')
+    .addAnswer('*oficina@gmtcarga.com*')
+    .addAnswer('*contabilidad@gmtcarga.com*')
+
+const flowOther = addKeyword(['3', 'asesor'])
+    .addAnswer('Entiendo en breve un asesor te dara respuesta')
+    .addAnswer(
+        [
+            "O recuerda nuestras opciones de consulta",
+            "👉🏻 1️⃣ 👈🏻. *Verificacion de pagos*",
+            "👉🏻 2️⃣ 👈🏻. *Certificados de Ingresos o Retenciones*",
+        ],
+        null,
+        null,
+        [flowVerifyPay, flowCertificados]
+    )
+    .addAnswer('*Gracias, por favor espera....*', {capture:true}, (ctx)=>{
+        if(ctx.body == 'asesor'){
+            return console.log(ctx.body)
+        }
+    })
+
+// const flowDefault = addKeyword('')
+//     .addAnswer('No entendemos tu solicitud')
+//     .addAnswer('Prueba escribiendo la palabra *Asesor*')
+//     .addAnswer(
+//         [
+//             "O recuerda nuestras opciones de consulta",
+//             "👉🏻 1️⃣ 👈🏻. *Verificacion de pagos*",
+//             "👉🏻 2️⃣ 👈🏻. *Certificados de Ingresos o Retenciones*",
+//         ],
+//         null,
+//         null,
+//         [flowVerifyPay, flowCertificados, flowOther]
+//     )
+
+const flowPrincipal = addKeyword(['hola','buenos dias', 'ole', 'alo'])
+    .addAnswer('🙌 Hola! Bienvenido a GMT transportes logisticos')
+    .addAnswer("Mi nombre es Stefani y te voy ayudar con tu solicitud",)
+    .addAnswer(
+        [
+            "Si necesitas ver más info sobre",
+            "👉🏻 1️⃣ 👈🏻. *Verificacion de pagos*",
+            "👉🏻 2️⃣ 👈🏻. *Certificados de Ingresos o Retenciones*",
+            "👉🏻 3️⃣ 👈🏻. *Otra consulta*"
+        ],
+        null,
+        null,
+        [flowVerifyPay, flowCertificados, flowOther]
+    )
+
+
+const main = async () => {
+    const adapterDB = new JsonFileAdapter()
+    const adapterFlow = createFlow([flowPrincipal, flowVerifyPay, flowCertificados, flowOther ])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
